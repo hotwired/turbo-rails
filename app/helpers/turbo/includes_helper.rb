89e234284c6316e75c85e9d52b0432ebdce3aac7
David Heinemeier Hansson <david@loudthinking.com>
2020-12-15 15:42:46 +0100

    Don't include the streams channel until its been converted

--- app/helpers/turbo/includes_helper.rb
+++ app/helpers/turbo/includes_helper.rb
@@ -1,6 +1,5 @@
 module Turbo::IncludesHelper
   def turbo_include_tags
-    javascript_include_tag("turbo", type: "module") + 
-    javascript_include_tag("turbo/updates_channel", type: "module")
+    javascript_include_tag("turbo", type: "module") # + javascript_include_tag("turbo/streams_channel", type: "module")
   end
 end
