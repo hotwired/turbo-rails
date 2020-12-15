1cad1864a38df1dfe4bba177a2034eb448a275bd
David Heinemeier Hansson <david@loudthinking.com>
2020-12-15 17:23:26 +0100

    Links become drive

--- app/helpers/turbo/drive_helper.rb
+++ app/helpers/turbo/drive_helper.rb
@@ -1,4 +1,4 @@
-module Turbo::LinksHelper
+module Turbo::DriveHelper
   # Pages that are more likely than not to be a cache miss can skip turbo cache to avoid visual jitter.
   # Note: This requires a +yield :head+ provision in the application layout.
   #
@@ -8,11 +8,9 @@ module Turbo::LinksHelper
   #   <html><head><%= yield :head %></head><body><%= yield %></html>
   #
   #   # app/views/trays/index.html.erb
-  #   <% turbo_links_exempts_page_from_cache %>
+  #   <% turbo_exempts_page_from_cache %>
   #   <p>Page that shouldn't be cached by Turbo</p>
-  #
-  # FIXME: turbolinks-cache-control -> turbo-cache-control
-  def turbo_links_exempts_page_from_cache
-    provide :head, %(<meta name="turbolinks-cache-control" content="no-cache">).html_safe
+  def turbo_exempts_page_from_cache
+    provide :head, %(<meta name="turbo-cache-control" content="no-cache">).html_safe
   end
 end
