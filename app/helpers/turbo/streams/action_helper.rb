0b7bc73db9127ea78970bb81f82a8b78d6a49766
David Heinemeier Hansson <david@loudthinking.com>
2020-12-18 12:24:24 +0100

    Document turbo_stream_action_tag

--- app/helpers/turbo/streams/action_helper.rb
+++ app/helpers/turbo/streams/action_helper.rb
@@ -1,4 +1,11 @@
 module Turbo::Streams::ActionHelper
+  # Creates a `turbo-stream` tag according to the passed parameters. Examples:
+  #
+  #   # => <turbo-stream action="remove" target="message_1"></turbo-stream>
+  #   turbo_stream_action_tag "remove", target: "message_1"
+  #
+  #   # => <turbo-stream action="replace" target="message_1"><template><div id="message_1">Hello!</div></template></turbo-stream>
+  #   turbo_stream_action_tag "replace", target: "message_1", template: %(<div id="message_1">Hello!</div>)
   def turbo_stream_action_tag(action, target:, template: nil)
     target   = convert_to_turbo_stream_dom_id(target)
     template = template ? "<template>#{template}</template>" : ""
