e5f32e5c0581e73ab708d9c1e8558364f4ffcc01
David Heinemeier Hansson <david@loudthinking.com>
2020-12-18 12:42:08 +0100

    Test all immediate broadcasts

--- test/streams/broadcastable_test.rb
+++ test/streams/broadcastable_test.rb
@@ -13,7 +13,7 @@ class Turbo::BroadcastableTest < ActionCable::Channel::TestCase
   end
 
   test "broadcasting remove now" do
-    assert_broadcast_on "message:1", turbo_stream_action_tag("remove", target: "message_1") do
+    assert_broadcast_on @message.to_param, turbo_stream_action_tag("remove", target: "message_1") do
       @message.broadcast_remove
     end
   end
@@ -21,6 +21,60 @@ class Turbo::BroadcastableTest < ActionCable::Channel::TestCase
   test "broadcasting replace to stream now" do
     assert_broadcast_on "stream", turbo_stream_action_tag("replace", target: "message_1", template: "<p>Hello!</p>") do
       @message.broadcast_replace_to "stream"
+    end
+  end
+
+  test "broadcasting replace now" do
+    assert_broadcast_on @message.to_param, turbo_stream_action_tag("replace", target: "message_1", template: "<p>Hello!</p>") do
+      @message.broadcast_replace
+    end
+  end
+
+  test "broadcasting append to stream now" do
+    assert_broadcast_on "stream", turbo_stream_action_tag("append", target: "messages", template: "<p>Hello!</p>") do
+      @message.broadcast_append_to "stream"
+    end
+  end
+
+  test "broadcasting append to stream with custom container now" do
+    assert_broadcast_on "stream", turbo_stream_action_tag("append", target: "board_messages", template: "<p>Hello!</p>") do
+      @message.broadcast_append_to "stream", container: "board_messages"
+    end
+  end
+
+  test "broadcasting append now" do
+    assert_broadcast_on @message.to_param, turbo_stream_action_tag("append", target: "messages", template: "<p>Hello!</p>") do
+      @message.broadcast_append
+    end
+  end
+
+  test "broadcasting prepend to stream now" do
+    assert_broadcast_on "stream", turbo_stream_action_tag("prepend", target: "messages", template: "<p>Hello!</p>") do
+      @message.broadcast_prepend_to "stream"
+    end
+  end
+
+  test "broadcasting prepend to stream with custom container now" do
+    assert_broadcast_on "stream", turbo_stream_action_tag("prepend", target: "board_messages", template: "<p>Hello!</p>") do
+      @message.broadcast_prepend_to "stream", container: "board_messages"
+    end
+  end
+
+  test "broadcasting prepend now" do
+    assert_broadcast_on @message.to_param, turbo_stream_action_tag("prepend", target: "messages", template: "<p>Hello!</p>") do
+      @message.broadcast_prepend
+    end
+  end
+
+  test "broadcasting action to stream now" do
+    assert_broadcast_on "stream", turbo_stream_action_tag("prepend", target: "messages", template: "<p>Hello!</p>") do
+      @message.broadcast_action_to "stream", action: "prepend"
+    end
+  end
+
+  test "broadcasting action now" do
+    assert_broadcast_on @message.to_param, turbo_stream_action_tag("prepend", target: "messages", template: "<p>Hello!</p>") do
+      @message.broadcast_action "prepend"
     end
   end
 end
