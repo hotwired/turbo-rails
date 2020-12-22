0d9bb0b63d081e3dc8d367ef2e990b264d5403b5
David Heinemeier Hansson <david@loudthinking.com>
2020-12-22 11:25:47 +0100

    Fixed that

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 # FIXME: Offer flag to opt out of these native routes
-# FIXME: Consider prefixing these URLs with "turbo/"
 Rails.application.routes.draw do
   get "recede_historical_location"  => "turbo/native/navigation#recede",  as: :turbo_recede_historical_location
   get "resume_historical_location"  => "turbo/native/navigation#resume",  as: :turbo_resume_historical_location
