480695ad8c51efe028f5040924dcb838216da07c
David Heinemeier Hansson <david@loudthinking.com>
2020-12-22 11:24:22 +0100

    Has to be listed first to take precedence

--- app/controllers/turbo/streams/turbo_streams_tag_builder.rb
+++ app/controllers/turbo/streams/turbo_streams_tag_builder.rb
@@ -6,8 +6,8 @@
 #     @user.destroy!
 #
 #     respond_to do |format|
-#       format.html         { redirect_to users_url, notice: "User removed" }
 #       format.turbo_stream { render turbo_stream: turbo_stream.remove(@user) }
+#       format.html         { redirect_to users_url, notice: "User removed" }
 #     end
 #   end
 #
