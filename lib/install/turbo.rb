e3fc9561e676d5870bdf773a785315ec333d96e7
Sen <sen9ob@gmail.com>
2020-12-23 17:21:24 +0800

    skip "Add Turbo include tags in application layout" when assets pipeline not used

--- lib/install/turbo.rb
+++ lib/install/turbo.rb
@@ -1,8 +1,10 @@
 say "Yield head in application layout for cache helper"
 insert_into_file Rails.root.join("app/views/layouts/application.html.erb").to_s, "\n    <%= yield :head %>", before: /\s*<\/head>/
 
-say "Add Turbo include tags in application layout"
-insert_into_file Rails.root.join("app/views/layouts/application.html.erb").to_s, "\n    <%= turbo_include_tags %>", before: /\s*<\/head>/
+if Rails.application.config.respond_to?(:assets)
+  say "Add Turbo include tags in application layout"
+  insert_into_file Rails.root.join("app/views/layouts/application.html.erb").to_s, "\n    <%= turbo_include_tags %>", before: /\s*<\/head>/
+end
 
 say "Enable redis in bundle"
 uncomment_lines "Gemfile", %(gem 'redis')
