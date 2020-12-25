bf00129d98f9f0bdd2289078c7ef2ab2782e0b3e
David Heinemeier Hansson <david@loudthinking.com>
2020-12-25 09:53:39 +0100

    Protect against missing default application layout on install
    Closes #14

--- lib/install/turbo.rb
+++ lib/install/turbo.rb
@@ -1,8 +1,15 @@
-say "Yield head in application layout for cache helper"
-insert_into_file Rails.root.join("app/views/layouts/application.html.erb").to_s, "\n    <%= yield :head %>", before: /\s*<\/head>/
+APPLICATION_LAYOUT_PATH = Rails.root.join("app/views/layouts/application.html.erb")
 
-say "Add Turbo include tags in application layout"
-insert_into_file Rails.root.join("app/views/layouts/application.html.erb").to_s, "\n    <%= turbo_include_tags %>", before: /\s*<\/head>/
+if APPLICATION_LAYOUT_PATH.exist?
+  say "Yield head in application layout for cache helper"
+  insert_into_file APPLICATION_LAYOUT_PATH.to_s, "\n    <%= yield :head %>", before: /\s*<\/head>/
+
+  say "Add Turbo include tags in application layout"
+  insert_into_file APPLICATION_LAYOUT_PATH.to_s, "\n    <%= turbo_include_tags %>", before: /\s*<\/head>/
+else
+  say "Default application.html.erb is missing!", :red
+  say "        Add <%= turbo_include_tags %> and <%= yield :head %> within the <head> tag in your custom layout."
+end
 
 say "Enable redis in bundle"
 uncomment_lines "Gemfile", %(gem 'redis')
