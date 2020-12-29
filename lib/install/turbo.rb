4b039fb48d545d9f08505b083a5c2eaad36dab4a
Tomas Valent <equivalent@eq8.eu>
2020-12-29 20:53:26 +0100

    Slim support for rails turbo:install to insert lines to application.html.slim

--- lib/install/turbo.rb
+++ lib/install/turbo.rb
@@ -1,12 +1,29 @@
-APPLICATION_LAYOUT_PATH = Rails.root.join("app/views/layouts/application.html.erb")
+APPLICATION_LAYOUT_PATH_ERB = Rails.root.join("app/views/layouts/application.html.erb")
+APPLICATION_LAYOUT_PATH_SLIM = Rails.root.join("app/views/layouts/application.html.slim")
 
-if APPLICATION_LAYOUT_PATH.exist?
+layout_file_exist = false
+
+if APPLICATION_LAYOUT_PATH_ERB.exist?
   say "Yield head in application layout for cache helper"
-  insert_into_file APPLICATION_LAYOUT_PATH.to_s, "\n    <%= yield :head %>", before: /\s*<\/head>/
+  insert_into_file APPLICATION_LAYOUT_PATH_ERB.to_s, "\n    <%= yield :head %>", before: /\s*<\/head>/
 
   say "Add Turbo include tags in application layout"
-  insert_into_file APPLICATION_LAYOUT_PATH.to_s, "\n    <%= turbo_include_tags %>", before: /\s*<\/head>/
-else
+  insert_into_file APPLICATION_LAYOUT_PATH_ERB.to_s, "\n    <%= turbo_include_tags %>", before: /\s*<\/head>/
+
+  layout_file_exist = true
+end
+
+if APPLICATION_LAYOUT_PATH_SLIM.exist?
+  say "Yield head in application layout for cache helper"
+  insert_into_file APPLICATION_LAYOUT_PATH_SLIM.to_s, "\n    = yield :head", after: /\s+head/
+
+  say "Add Turbo include tags in application layout"
+
+  insert_into_file APPLICATION_LAYOUT_PATH_SLIM.to_s, "\n    = turbo_include_tags", after: /\s+head/
+  layout_file_exist = true
+end
+
+unless layout_file_exist
   say "Default application.html.erb is missing!", :red
   say "        Add <%= turbo_include_tags %> and <%= yield :head %> within the <head> tag in your custom layout."
 end
