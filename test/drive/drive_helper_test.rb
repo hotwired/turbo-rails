8905f75b2c942331758c389e333148fbfc11aa3c
Danielius <danieliusvisockas@gmail.com>
2021-01-11 17:32:58 +0200

    Add test case for templates without html postfix

--- test/drive/drive_helper_test.rb
+++ test/drive/drive_helper_test.rb
@@ -5,4 +5,9 @@ class Turbo::DriveHelperTest < ActionDispatch::IntegrationTest
     get trays_path
     assert_select "meta", name: "turbo-cache-control", content: "no-cache"
   end
+
+  test "rendering template as html returns non-turbo mime type" do
+    get trays_path, as: :turbo_stream
+    refute_includes response.content_type, "turbo-stream"
+  end
 end
