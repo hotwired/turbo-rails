9fae9ef64f1e21ebc71cd2bf3a4703d9ab8089b2
Kasper Timm Hansen <kaspth@gmail.com>
2021-01-29 21:58:52 +0100

    Use a more real name

--- test/dummy/app/controllers/users/profiles_controller.rb
+++ test/dummy/app/controllers/users/profiles_controller.rb
@@ -1,5 +1,5 @@
 class Users::ProfilesController < ApplicationController
   def show
-    @profile = Users::Profile.new(id: 1, name: "User")
+    @profile = Users::Profile.new(id: 1, name: "David")
   end
 end
