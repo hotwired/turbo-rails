e1f46d799e859fea115b72479f53abc634ca4061
Bram Jetten <mail@bramjetten.nl>
2021-01-29 21:48:22 +0100

    Use assert_dom_equals in test

--- test/streams/streams_controller_test.rb
+++ test/streams/streams_controller_test.rb
@@ -40,9 +40,9 @@ STREAM
   
   test "render correct partial for namespaced models" do
     get users_profile_path(id: 1), as: :turbo_stream
-    assert_equal <<-STREAM, @response.body
+    assert_dom_equal <<~HTML, @response.body
 <turbo-stream action="replace" target="users_profile_1"><template><p>User</p></template></turbo-stream>
 <turbo-stream action="update" target="users_profile_1"><template><p>User</p></template></turbo-stream>
-STREAM
+    HTML
   end
 end
