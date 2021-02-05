405c24fb2f670465fad43735adcc5bebfa36d05f
Ankur Patel <ankur@encoredevlabs.com>
2021-02-05 11:17:37 -0500

    Allow rendering to be passed so that we can change the partial and object when broadcasting

--- app/models/concerns/turbo/broadcastable.rb
+++ app/models/concerns/turbo/broadcastable.rb
@@ -51,10 +51,10 @@ module Turbo::Broadcastable
     #     belongs_to :board
     #     broadcasts_to ->(message) { [ message.board, :messages ] }, inserts_by: :prepend, target: "board_messages"
     #   end
-    def broadcasts_to(stream, inserts_by: :append, target: broadcast_target_default)
-      after_create_commit  -> { broadcast_action_later_to stream.try(:call, self) || send(stream), action: inserts_by, target: target }
-      after_update_commit  -> { broadcast_replace_later_to stream.try(:call, self) || send(stream) }
-      after_destroy_commit -> { broadcast_remove_to stream.try(:call, self) || send(stream) }
+    def broadcasts_to(stream, inserts_by: :append, target: broadcast_target_default, **rendering)
+      after_create_commit  -> { broadcast_action_later_to stream.try(:call, self) || send(stream), action: inserts_by, target: target, **rendering }
+      after_update_commit  -> { broadcast_replace_later_to stream.try(:call, self) || send(stream), **rendering }
+      after_destroy_commit -> { broadcast_remove_to stream.try(:call, self) || send(stream), **rendering }
     end
 
     # Same as <tt>#broadcasts_to</tt>, but the designated stream is automatically set to the current model.
