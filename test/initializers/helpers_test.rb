137bf25f4486f9ab5e478e172f4da7649b74b1f4
Xavier Noria <fxn@hashref.com>
2021-02-28 23:24:57 +0100

    run turbo.helpers before config/initializers

---
+++ test/initializers/helpers_test.rb
@@ -0,0 +1,8 @@
+require "turbo_test"
+
+class Turbo::HelpersInInitializersTest < ActionDispatch::IntegrationTest
+  test "AC::Base has the helpers in place when initializers run" do
+    assert_includes $dummy_ac_base_ancestors_in_initializers, Turbo::Streams::TurboStreamsTagBuilder
+    assert_includes $dummy_ac_base_helpers_in_initializers, Turbo::StreamsHelper
+  end
+end
