fc168ed2de6f24b3b9b64cd5cc6cf6f9f208fcff
Simon Fish <si@mon.fish>
2021-05-27 16:05:00 +0100

    Make config/cable.yml optional during setup

--- lib/install/turbo_with_asset_pipeline.rb
+++ lib/install/turbo_with_asset_pipeline.rb
@@ -1,5 +1,6 @@
 APPLICATION_LAYOUT_PATH = Rails.root.join("app/views/layouts/application.html.erb")
 IMPORTMAP_PATH = Rails.root.join("app/assets/javascripts/importmap.json.erb")
+CABLE_CONFIG_PATH = Rails.root.join("config/cable.yml")
 
 if APPLICATION_LAYOUT_PATH.exist?
   say "Yield head in application layout for cache helper"
@@ -22,10 +23,14 @@ else
   say %(        Add <%= javascript_include_tag("turbo", type: "module-shim") %> and <%= yield :head %> within the <head> tag after Stimulus includes in your custom layout.)
 end
 
-say "Enable redis in bundle"
-uncomment_lines "Gemfile", %(gem 'redis')
+if CABLE_CONFIG_PATH.exist?
+  say "Enable redis in bundle"
+  uncomment_lines "Gemfile", %(gem 'redis')
 
-say "Switch development cable to use redis"
-gsub_file "config/cable.yml", /development:\n\s+adapter: async/, "development:\n  adapter: redis\n  url: redis://localhost:6379/1"
+  say "Switch development cable to use redis"
+  gsub_file CABLE_CONFIG_PATH.to_s, /development:\n\s+adapter: async/, "development:\n  adapter: redis\n  url: redis://localhost:6379/1"
+else
+  say 'ActionCable config file (config/cable.yml) is missing. Uncomment "gem \'redis\'" in your Gemfile and create config/cable.yml to use the Turbo Streams broadcast feature.'
+end
 
 say "Turbo successfully installed ⚡️", :green
--- lib/install/turbo_with_webpacker.rb
+++ lib/install/turbo_with_webpacker.rb
@@ -1,6 +1,7 @@
 # Some Rails versions use commonJS(require) others use ESM(import).
 TURBOLINKS_REGEX = /(import .* from "turbolinks".*\n|require\("turbolinks"\).*\n)/.freeze
 ACTIVE_STORAGE_REGEX = /(import.*ActiveStorage|require.*@rails\/activestorage.*)/.freeze
+CABLE_CONFIG_PATH = Rails.root.join("config/cable.yml")
 
 abort "❌ Webpacker not found. Exiting." unless defined?(Webpacker::Engine)
 
@@ -15,10 +16,14 @@ run "#{RbConfig.ruby} bin/yarn remove turbolinks"
 gsub_file "#{Webpacker.config.source_entry_path}/application.js", TURBOLINKS_REGEX, ''
 gsub_file "#{Webpacker.config.source_entry_path}/application.js", /Turbolinks.start.*\n/, ''
 
-say "Enable redis in bundle"
-uncomment_lines "Gemfile", %(gem 'redis')
+if CABLE_CONFIG_PATH.exist?
+  say "Enable redis in bundle"
+  uncomment_lines "Gemfile", %(gem 'redis')
 
-say "Switch development cable to use redis"
-gsub_file "config/cable.yml", /development:\n\s+adapter: async/, "development:\n  adapter: redis\n  url: redis://localhost:6379/1"
+  say "Switch development cable to use redis"
+  gsub_file CABLE_CONFIG_PATH.to_s, /development:\n\s+adapter: async/, "development:\n  adapter: redis\n  url: redis://localhost:6379/1"
+else
+  say 'ActionCable config file (config/cable.yml) is missing. Uncomment "gem \'redis\'" in your Gemfile and create config/cable.yml to use the Turbo Streams broadcast feature.'
+end
 
 say "Turbo successfully installed ⚡️", :green
