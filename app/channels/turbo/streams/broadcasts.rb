ef48bde1587b6e05cc02f72ffa9b3088dfe36ba6
Jose Galisteo <ceritium@gmail.com>
2021-08-26 17:35:05 +0200

    Turbo::StreamsChannel can set targets attribute on stream tag (#210)
    * Turbo::StreamsChannel can set targets attribute
    
    Background:
    - https://github.com/hotwired/turbo/pull/113 allowed for multiple targets in turbo frame streams.
    - https://github.com/hotwired/turbo-rails/pull/194 implemented it for `Turbo::Streams::TagBuilder`
    
    This attempts to expose the feature on `Turbo::StreamsChannel` methods.
    
    * Add assertions for `targets` argument

--- app/channels/turbo/streams/broadcasts.rb
+++ app/channels/turbo/streams/broadcasts.rb
@@ -5,70 +5,68 @@
 module Turbo::Streams::Broadcasts
   include Turbo::Streams::ActionHelper
 
-  def broadcast_remove_to(*streamables, target:)
-    broadcast_action_to *streamables, action: :remove, target: target
+  def broadcast_remove_to(*streamables, **opts)
+    broadcast_action_to *streamables, action: :remove, **opts
   end
 
-  def broadcast_replace_to(*streamables, target:, **rendering)
-    broadcast_action_to *streamables, action: :replace, target: target, **rendering
+  def broadcast_replace_to(*streamables, **opts)
+    broadcast_action_to *streamables, action: :replace, **opts
   end
 
-  def broadcast_update_to(*streamables, target:, **rendering)
-    broadcast_action_to *streamables, action: :update, target: target, **rendering
+  def broadcast_update_to(*streamables, **opts)
+    broadcast_action_to *streamables, action: :update, **opts
   end
 
-  def broadcast_before_to(*streamables, target:, **rendering)
-    broadcast_action_to *streamables, action: :before, target: target, **rendering
+  def broadcast_before_to(*streamables, **opts)
+    broadcast_action_to *streamables, action: :before, **opts
   end
 
-  def broadcast_after_to(*streamables, target:, **rendering)
-    broadcast_action_to *streamables, action: :after, target: target, **rendering
+  def broadcast_after_to(*streamables, **opts)
+    broadcast_action_to *streamables, action: :after, **opts
   end
 
-  def broadcast_append_to(*streamables, target:, **rendering)
-    broadcast_action_to *streamables, action: :append, target: target, **rendering
+  def broadcast_append_to(*streamables, **opts)
+    broadcast_action_to *streamables, action: :append, **opts
   end
 
-  def broadcast_prepend_to(*streamables, target:, **rendering)
-    broadcast_action_to *streamables, action: :prepend, target: target, **rendering
+  def broadcast_prepend_to(*streamables, **opts)
+    broadcast_action_to *streamables, action: :prepend, **opts
   end
 
-  def broadcast_action_to(*streamables, action:, target:, **rendering)
-    broadcast_stream_to *streamables, content: turbo_stream_action_tag(action, target: target, template:
+  def broadcast_action_to(*streamables, action:, target: nil, targets: nil, **rendering)
+    broadcast_stream_to *streamables, content: turbo_stream_action_tag(action, target: target, targets: targets, template:
       rendering.delete(:content) || (rendering.any? ? render_format(:html, **rendering) : nil)
     )
   end
 
-
-  def broadcast_replace_later_to(*streamables, target:, **rendering)
-    broadcast_action_later_to *streamables, action: :replace, target: target, **rendering
+  def broadcast_replace_later_to(*streamables, **opts)
+    broadcast_action_later_to *streamables, action: :replace, **opts
   end
 
-  def broadcast_update_later_to(*streamables, target:, **rendering)
-    broadcast_action_later_to *streamables, action: :update, target: target, **rendering
+  def broadcast_update_later_to(*streamables, **opts)
+    broadcast_action_later_to *streamables, action: :update, **opts
   end
 
-  def broadcast_before_later_to(*streamables, target:, **rendering)
-    broadcast_action_later_to *streamables, action: :before, target: target, **rendering
+  def broadcast_before_later_to(*streamables, **opts)
+    broadcast_action_later_to *streamables, action: :before, **opts
   end
 
-  def broadcast_after_later_to(*streamables, target:, **rendering)
-    broadcast_action_later_to *streamables, action: :after, target: target, **rendering
+  def broadcast_after_later_to(*streamables, **opts)
+    broadcast_action_later_to *streamables, action: :after, **opts
   end
 
-  def broadcast_append_later_to(*streamables, target:, **rendering)
-    broadcast_action_later_to *streamables, action: :append, target: target, **rendering
+  def broadcast_append_later_to(*streamables, **opts)
+    broadcast_action_later_to *streamables, action: :append, **opts
   end
 
-  def broadcast_prepend_later_to(*streamables, target:, **rendering)
-    broadcast_action_later_to *streamables, action: :prepend, target: target, **rendering
+  def broadcast_prepend_later_to(*streamables, **opts)
+    broadcast_action_later_to *streamables, action: :prepend, **opts
   end
 
-  def broadcast_action_later_to(*streamables, action:, target:, **rendering)
+  def broadcast_action_later_to(*streamables, action:, target: nil, targets: nil, **rendering)
     Turbo::Streams::ActionBroadcastJob.perform_later \
-      stream_name_from(streamables), action: action, target: target, **rendering
+      stream_name_from(streamables), action: action, target: target, targets: targets, **rendering
   end
-
 
   def broadcast_render_to(*streamables, **rendering)
     broadcast_stream_to *streamables, content: render_format(:turbo_stream, **rendering)
