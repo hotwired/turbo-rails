5c5a85da8aafe3a7af919a8b8e05daafd53fab60
David Heinemeier Hansson <david@loudthinking.com>
2021-09-03 12:36:09 +0200

    Guard against missing default entrypoint

--- lib/install/turbo_with_node.rb
+++ lib/install/turbo_with_node.rb
@@ -1,5 +1,9 @@
-say "Import Turbo"
-append_to_file "app/javascript/application.js", %(import "@hotwired/turbo-rails"\n)
+if (js_entrypoint_path = Rails.root.join("app/javascript/application.js")).exist?
+  say "Import Turbo"
+  append_to_file "app/javascript/application.js", %(import "@hotwired/turbo-rails"\n)
+else
+  say "You must import @hotwired/turbo-rails in your JavaScript entrypoint file", :red
+end
 
 say "Install Turbo"
 run "npm i @hotwired/turbo-rails"
