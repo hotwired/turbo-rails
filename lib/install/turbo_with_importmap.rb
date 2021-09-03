deec27477e69d2561d03814489172c437883be42
David Heinemeier Hansson <david@loudthinking.com>
2021-09-03 12:35:54 +0200

    Fix path

--- lib/install/turbo_with_importmap.rb
+++ lib/install/turbo_with_importmap.rb
@@ -2,4 +2,4 @@ say "Import Turbo"
 append_to_file "app/javascript/application.js", %(import "@hotwired/turbo-rails"\n)
 
 say "Pin Turbo"
-append_to_file importmap_path.to_s, %(pin "@hotwired/turbo-rails", to: "turbo.js"\n)
+append_to_file "config/importmap.rb", %(pin "@hotwired/turbo-rails", to: "turbo.js"\n)
