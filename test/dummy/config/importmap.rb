2ab63b939ad34f7064413c7d9e93de6058d7e706
Sean Doyle <sean.p.doyle24@gmail.com>
2021-09-10 12:43:21 -0400

    Depend on `importmap-rails` in dummy app
    When running tests against a dummy Rails application, use
    [importmap-rails][].
    
    The contents of this commit were generated by following the
    [Installation][] instructions.
    
    [importmap-rails]: https://github.com/rails/importmap-rails/tree/v0.5.1
    [Installation]: https://github.com/rails/importmap-rails/tree/v0.5.1#installation

---
+++ test/dummy/config/importmap.rb
@@ -0,0 +1,6 @@
+# Use direct uploads for Active Storage (remember to import "@rails/activestorage" in your application.js)
+# pin "@rails/activestorage", to: "activestorage.esm.js"
+
+# Use node modules from a JavaScript CDN by running ./bin/importmap
+
+pin "application"
