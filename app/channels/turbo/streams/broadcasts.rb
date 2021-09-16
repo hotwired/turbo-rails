36735a51cf89ab4649ae726c6bb7b1102be90ff4
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2021-09-16 23:03:52 +0900

    Suppress warnings with `ruby -w`
    I got the following warnings from turbo-rails when I tried Rails 7.0.0.alpha2.
    
    ```
    /path/to/gems/turbo-rails-0.7.12/app/models/concerns/turbo/broadcastable.rb:79: warning: `*' interpreted as argument prefix
    /path/to/gems/turbo-rails-0.7.12/app/models/concerns/turbo/broadcastable.rb:98: warning: `*' interpreted as argument prefix
    /path/to/gems/turbo-rails-0.7.12/app/models/concerns/turbo/broadcastable.rb:117: warning: `*' interpreted as argument prefix
    /path/to/gems/turbo-rails-0.7.12/app/models/concerns/turbo/broadcastable.rb:138: warning: `*' interpreted as argument prefix
    /path/to/gems/turbo-rails-0.7.12/app/models/concerns/turbo/broadcastable.rb:154: warning: `*' interpreted as argument prefix
    /path/to/gems/turbo-rails-0.7.12/app/models/concerns/turbo/broadcastable.rb:170: warning: `*' interpreted as argument prefix
    /path/to/gems/turbo-rails-0.7.12/app/models/concerns/turbo/broadcastable.rb:191: warning: `*' interpreted as argument prefix
    /path/to/gems/turbo-rails-0.7.12/app/models/concerns/turbo/broadcastable.rb:216: warning: `*' interpreted as argument prefix
    /path/to/gems/turbo-rails-0.7.12/app/models/concerns/turbo/broadcastable.rb:226: warning: `*' interpreted as argument prefix
    /path/to/gems/turbo-rails-0.7.12/app/models/concerns/turbo/broadcastable.rb:236: warning: `*' interpreted as argument prefix
    /path/to/gems/turbo-rails-0.7.12/app/models/concerns/turbo/broadcastable.rb:246: warning: `*' interpreted as argument prefix
    /path/to/gems/turbo-rails-0.7.12/app/models/concerns/turbo/broadcastable.rb:286: warning: `*' interpreted as argument prefix
    ```
    
    This patch suppresses the warnings and others which I found in this
    repository with `ls **/*.rb | xargs -n1 ruby -cw`. Now the command
    displays "Syntax OK" only.

--- app/channels/turbo/streams/broadcasts.rb
+++ app/channels/turbo/streams/broadcasts.rb
@@ -6,61 +6,61 @@ module Turbo::Streams::Broadcasts
   include Turbo::Streams::ActionHelper
 
   def broadcast_remove_to(*streamables, **opts)
-    broadcast_action_to *streamables, action: :remove, **opts
+    broadcast_action_to(*streamables, action: :remove, **opts)
   end
 
   def broadcast_replace_to(*streamables, **opts)
-    broadcast_action_to *streamables, action: :replace, **opts
+    broadcast_action_to(*streamables, action: :replace, **opts)
   end
 
   def broadcast_update_to(*streamables, **opts)
-    broadcast_action_to *streamables, action: :update, **opts
+    broadcast_action_to(*streamables, action: :update, **opts)
   end
 
   def broadcast_before_to(*streamables, **opts)
-    broadcast_action_to *streamables, action: :before, **opts
+    broadcast_action_to(*streamables, action: :before, **opts)
   end
 
   def broadcast_after_to(*streamables, **opts)
-    broadcast_action_to *streamables, action: :after, **opts
+    broadcast_action_to(*streamables, action: :after, **opts)
   end
 
   def broadcast_append_to(*streamables, **opts)
-    broadcast_action_to *streamables, action: :append, **opts
+    broadcast_action_to(*streamables, action: :append, **opts)
   end
 
   def broadcast_prepend_to(*streamables, **opts)
-    broadcast_action_to *streamables, action: :prepend, **opts
+    broadcast_action_to(*streamables, action: :prepend, **opts)
   end
 
   def broadcast_action_to(*streamables, action:, target: nil, targets: nil, **rendering)
-    broadcast_stream_to *streamables, content: turbo_stream_action_tag(action, target: target, targets: targets, template:
+    broadcast_stream_to(*streamables, content: turbo_stream_action_tag(action, target: target, targets: targets, template:
       rendering.delete(:content) || (rendering.any? ? render_format(:html, **rendering) : nil)
-    )
+    ))
   end
 
   def broadcast_replace_later_to(*streamables, **opts)
-    broadcast_action_later_to *streamables, action: :replace, **opts
+    broadcast_action_later_to(*streamables, action: :replace, **opts)
   end
 
   def broadcast_update_later_to(*streamables, **opts)
-    broadcast_action_later_to *streamables, action: :update, **opts
+    broadcast_action_later_to(*streamables, action: :update, **opts)
   end
 
   def broadcast_before_later_to(*streamables, **opts)
-    broadcast_action_later_to *streamables, action: :before, **opts
+    broadcast_action_later_to(*streamables, action: :before, **opts)
   end
 
   def broadcast_after_later_to(*streamables, **opts)
-    broadcast_action_later_to *streamables, action: :after, **opts
+    broadcast_action_later_to(*streamables, action: :after, **opts)
   end
 
   def broadcast_append_later_to(*streamables, **opts)
-    broadcast_action_later_to *streamables, action: :append, **opts
+    broadcast_action_later_to(*streamables, action: :append, **opts)
   end
 
   def broadcast_prepend_later_to(*streamables, **opts)
-    broadcast_action_later_to *streamables, action: :prepend, **opts
+    broadcast_action_later_to(*streamables, action: :prepend, **opts)
   end
 
   def broadcast_action_later_to(*streamables, action:, target: nil, targets: nil, **rendering)
@@ -69,7 +69,7 @@ module Turbo::Streams::Broadcasts
   end
 
   def broadcast_render_to(*streamables, **rendering)
-    broadcast_stream_to *streamables, content: render_format(:turbo_stream, **rendering)
+    broadcast_stream_to(*streamables, content: render_format(:turbo_stream, **rendering))
   end
 
   def broadcast_render_later_to(*streamables, **rendering)
