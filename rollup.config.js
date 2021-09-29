3e4b586c0fcaaec13dbff7471f9b5b5ee718833a
David Heinemeier Hansson <david@loudthinking.com>
2021-09-29 08:25:10 -0700

    Produce a minified output as well

--- rollup.config.js
+++ rollup.config.js
@@ -2,22 +2,40 @@ import resolve from "@rollup/plugin-node-resolve"
 import { terser } from "rollup-plugin-terser"
 import pkg from "./package.json"
 
-export default {
-  input: pkg.module,
-  output: {
-    file: pkg.main,
-    format: "es",
-    inlineDynamicImports: true
+export default [
+  {
+    input: pkg.module,
+    output: {
+      file: pkg.main,
+      format: "es",
+      inlineDynamicImports: true
+    },
+    plugins: [
+      resolve(),
+      terser({
+        mangle: false,
+        compress: false,
+        format: {
+          beautify: true,
+          indent_level: 2
+        }
+      })
+    ]
   },
-  plugins: [
-    resolve(),
-    terser({
-      mangle: false,
-      compress: false,
-      format: {
-        beautify: true,
-        indent_level: 2
-      }
-    })
-  ]
-}
+
+  {
+    input: pkg.module,
+    output: {
+      file: "app/assets/javascripts/turbo.min.js",
+      format: "es",
+      inlineDynamicImports: true
+    },
+    plugins: [
+      resolve(),
+      terser({
+        mangle: true,
+        compress: true
+      })
+    ]
+  }
+]
