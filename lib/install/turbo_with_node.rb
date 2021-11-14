301d7b73e1cf45bb66fc5c74c8cb43e7cceb3946
Pito Salas <pitosalas@users.noreply.github.com>
2021-11-14 17:58:11 -0500

    Edit error message for clarity
    See https://github.com/hotwired/hotwire-rails/issues/45

--- lib/install/turbo_with_node.rb
+++ lib/install/turbo_with_node.rb
@@ -2,7 +2,7 @@ if (js_entrypoint_path = Rails.root.join("app/javascript/application.js")).exist
   say "Import Turbo"
   append_to_file "app/javascript/application.js", %(import "@hotwired/turbo-rails"\n)
 else
-  say "You must import @hotwired/turbo-rails in your JavaScript entrypoint file", :red
+  say %(For Rails 6, add "import '@hotwired/turbo-rails'" to "app/javascript/packs/application.js"), :red
 end
 
 say "Install Turbo"
