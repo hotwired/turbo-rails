37be2c6c39c417ddd5f12dcd494fb232220b0d08
Sean Doyle <seanpdoyle@users.noreply.github.com>
2022-05-24 08:28:46 -0400

    Replace `?` operator with conditional check (#337)
    Closes [#336][]
    
    Address downstream Rails consumers' webpack syntax issues by replacing
    the `?` operator with a check that `submitter` is present.
    
    [#336]: https://github.com/hotwired/turbo-rails/issues/336

--- app/javascript/turbo/form_submissions.js
+++ app/javascript/turbo/form_submissions.js
@@ -1,7 +1,5 @@
 export function overrideMethodWithFormmethod({ detail: { formSubmission: { fetchRequest, submitter } } }) {
-  const formMethod = submitter?.formMethod
-
-  if (formMethod && fetchRequest.body.has("_method")) {
-    fetchRequest.body.set("_method", formMethod)
+  if (submitter && submitter.formMethod && fetchRequest.body.has("_method")) {
+    fetchRequest.body.set("_method", submitter.formMethod)
   }
 }
