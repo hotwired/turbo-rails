43bf84a9b1d2cc78da6810b82f92be2c32c9dbfd
Alex Robbin <agrobbin@gmail.com>
2022-07-15 14:56:52 -0400

    enhance provided Drive helpers to allow controlling `cache` and `visit` meta tags (#360)

--- app/helpers/turbo/drive_helper.rb
+++ app/helpers/turbo/drive_helper.rb
@@ -1,6 +1,5 @@
 module Turbo::DriveHelper
-  # Pages that are more likely than not to be a cache miss can skip turbo cache to avoid visual jitter.
-  # Note: This requires a +yield :head+ provision in the application layout.
+  # Note: These helpers require a +yield :head+ provision in the layout.
   #
   # ==== Example
   #
@@ -10,7 +9,21 @@ module Turbo::DriveHelper
   #   # app/views/trays/index.html.erb
   #   <% turbo_exempts_page_from_cache %>
   #   <p>Page that shouldn't be cached by Turbo</p>
+
+  # Pages that are more likely than not to be a cache miss can skip turbo cache to avoid visual jitter.
+  # Cannot be used along with +turbo_exempts_page_from_preview+.
   def turbo_exempts_page_from_cache
-    provide :head, %(<meta name="turbo-cache-control" content="no-cache">).html_safe
+    provide :head, tag.meta(name: "turbo-cache-control", content: "no-cache")
+  end
+
+  # Specify that a cached version of the page should not be shown as a preview during an application visit.
+  # Cannot be used along with +turbo_exempts_page_from_cache+.
+  def turbo_exempts_page_from_preview
+    provide :head, tag.meta(name: "turbo-cache-control", content: "no-preview")
+  end
+
+  # Force the page, when loaded by Turbo, to be cause a full page reload.
+  def turbo_page_requires_reload
+    provide :head, tag.meta(name: "turbo-visit-control", content: "reload")
   end
 end
