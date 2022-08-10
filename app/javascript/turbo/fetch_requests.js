8b767b133c9527c5d8199dbe878390695361011a
Sean Doyle <sean.p.doyle24@gmail.com>
2022-08-10 17:02:25 -0400

    Encode HTTP method into Request body as `_method`
    Browsers do not handle `<form>` element submissions with a `[method]`
    value other than `get` or `post`. This also applies to `<form>` elements
    submitted with submitter elements that declare `[formmethod]` values
    other than `get` or `post`.
    
    To circumvent that constraint, Rails form builders encode other verbs
    into a `_method` parameter that is forwarded to the server.
    
    On the other hand, Turbo's reliance on [fetch][] means that it can
    lift `[method]` or `[formmethod]` values directly to `fetch` calls,
    regardless of verb.
    
    The way that Rails responds to `DELETE` or `PATCH` requests submitted in
    that manner differs from those with `_method=DELETE` or `_method=PATCH`.
    Primarily, controllers that call `redirect_to` default to using a [302
    Found][] status. When a Turbo request with `PATCH` receives a `302`
    response, the subsequent request to the resource is _also made with the
    PATCH verb_, often resulting in unintended side-effects or unhandled
    requests.
    
    The Turbo documentation suggests responding to those requests with [303
    See Other][] instead. This is sound advice, and _should_ be applied.
    
    However, for Rails applications migrating to Turbo from Unobtrusive
    JavaScript, making that change application-wide can be demanding.
    
    For reasons outside the scope of the pull request, Rails' internal
    machinery redirects requests made with `POST` verbs that encode a
    `_method` parameter in a way that doesn't require a `303`.
    
    This commit transforms the existing `turbo:submit-start` event listener
    into a `turbo:before-fetch-request` listener that transforms requests
    with methods other than `POST` into `POST` requests that encode their
    methods as `_method`.
    
    The net result is that migrating applications can gradually introduce
    `303 See Other` redirects at their own pace.
    
    [_method]: https://guides.rubyonrails.org/form_helpers.html#how-do-forms-with-patch-put-or-delete-methods-work-questionmark
    [fetch]: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
    [302 Found]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/302
    [303 See Other]: https://turbo.hotwired.dev/handbook/drive#redirecting-after-a-form-submission

---
+++ app/javascript/turbo/fetch_requests.js
@@ -0,0 +1,19 @@
+export function encodeMethodIntoRequestBody(event) {
+  if (event.target instanceof HTMLFormElement) {
+    const { target: form, detail: { fetchOptions } } = event
+
+    form.addEventListener("turbo:submit-start", ({ detail: { formSubmission: { submitter } } }) => {
+      const method = (submitter && submitter.formMethod) || fetchOptions.body.get("_method") || form.getAttribute("method")
+
+      if (!/get/i.test(method)) {
+        if (/post/i.test(method)) {
+          fetchOptions.body.delete("_method")
+        } else {
+          fetchOptions.body.set("_method", method)
+        }
+
+        fetchOptions.method = "post"
+      }
+    }, { once: true })
+  }
+}
