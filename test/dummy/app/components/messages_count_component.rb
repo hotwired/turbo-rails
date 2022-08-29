432b915b0bdbae90258afde187f404859658f9b8
Joshua Klina <joshua.klina@gmail.com>
2022-08-29 10:27:53 -0400

    Remove reliance on ViewComponent library

--- test/dummy/app/components/messages_count_component.rb
+++ test/dummy/app/components/messages_count_component.rb
@@ -1,9 +1,13 @@
-class MessagesCountComponent < ViewComponent::Base
+class MessagesCountComponent
   def initialize(count:)
     @count = count
   end
 
-  def call
+  def render_in(_view_context)
     "#{@count} messages sent from component"
+  end
+
+  def format
+    :html
   end
 end
