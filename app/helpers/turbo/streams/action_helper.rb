50fb92fe2d773cfce503613da0e4f67cb02f7c0d
Marco Roth <marco.roth@intergga.ch>
2022-09-13 15:58:42 +0200

    Allow additional attributes on the `turbo_stream_action_tag` helper (#373)
    * Allow additional attributes on the `turbo_stream_action_tag` helper
    
    * Remove unnecessary `Hash#compact` call

--- app/helpers/turbo/streams/action_helper.rb
+++ app/helpers/turbo/streams/action_helper.rb
@@ -1,4 +1,6 @@
 module Turbo::Streams::ActionHelper
+  include ActionView::Helpers::TagHelper
+
   # Creates a `turbo-stream` tag according to the passed parameters. Examples:
   #
   #   turbo_stream_action_tag "remove", target: "message_1"
@@ -9,13 +11,13 @@ module Turbo::Streams::ActionHelper
   #
   #   turbo_stream_action_tag "replace", targets: "message_1", template: %(<div id="message_1">Hello!</div>)
   #   # => <turbo-stream action="replace" targets="message_1"><template><div id="message_1">Hello!</div></template></turbo-stream>
-  def turbo_stream_action_tag(action, target: nil, targets: nil, template: nil)
-    template = action.to_sym == :remove ? "" : "<template>#{template}</template>"
+  def turbo_stream_action_tag(action, target: nil, targets: nil, template: nil, **attributes)
+    template = action.to_sym == :remove ? "" : tag.template(template.to_s.html_safe)
 
     if target = convert_to_turbo_stream_dom_id(target)
-      %(<turbo-stream action="#{action}" target="#{target}">#{template}</turbo-stream>).html_safe
+      tag.turbo_stream(template, **attributes.merge(action: action, target: target))
     elsif targets = convert_to_turbo_stream_dom_id(targets, include_selector: true)
-      %(<turbo-stream action="#{action}" targets="#{targets}">#{template}</turbo-stream>).html_safe
+      tag.turbo_stream(template, **attributes.merge(action: action, targets: targets))
     else
       raise ArgumentError, "target or targets must be supplied"
     end
