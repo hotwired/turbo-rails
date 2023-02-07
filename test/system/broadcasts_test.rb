421884bc1e7808c7076a1703e422add5e4d8bfd6
Kevin McConnell <kevin@37signals.com>
2023-02-07 16:43:15 +0000

    Add connected state to `turbo-cable-stream-source`
    When a `turbo-cable-stream-source` is added to a page, it asynchronously
    opens a subscription to the appropriate stream. However it did not
    provide a way to programmatically tell if/when that subscription was
    connected.
    
    This makes it hard to write reliable tests that depend on streams being
    ready for use. Or to expose that connection state in the UI (for
    example, to show a warning when connection is lost).
    
    To address this, we can add a `connected` attribute to the
    `turbo-cable-stream-source` element whenever it is connected.
    
    Using this, we can fix the flakiness in our broadcast tests, as now they
    can reliably wait for a `turbo-cable-stream-source[connected]` to be on
    the page before attempting to broadcast anything.

--- test/system/broadcasts_test.rb
+++ test/system/broadcasts_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class BroadcastsTest < ApplicationSystemTestCase
   test "Message broadcasts Turbo Streams" do
     visit messages_path
-    subscribe_to_broadcasts
+    wait_for_stream_to_be_connected
 
     assert_broadcasts_text "Message 1", to: :messages do |text, target|
       Message.create(content: text).broadcast_append_to(target)
@@ -12,7 +12,7 @@ class BroadcastsTest < ApplicationSystemTestCase
 
   test "Message broadcasts with html: render option" do
     visit messages_path
-    subscribe_to_broadcasts
+    wait_for_stream_to_be_connected
 
     assert_broadcasts_text "Hello, with html: option", to: :messages do |text, target|
       Message.create(content: "Ignored").broadcast_append_to(target, html: text)
@@ -21,7 +21,7 @@ class BroadcastsTest < ApplicationSystemTestCase
 
   test "Users::Profile broadcasts Turbo Streams" do
     visit users_profiles_path
-    subscribe_to_broadcasts
+    wait_for_stream_to_be_connected
 
     assert_broadcasts_text "Profile 1", to: :users_profiles do |text, target|
       Users::Profile.new(id: 1, name: text).broadcast_append_to(target)
@@ -29,21 +29,18 @@ class BroadcastsTest < ApplicationSystemTestCase
   end
 
   test "passing extra parameters to channel" do
-    visit echo_messages_path
+    visit section_messages_path
+    wait_for_stream_to_be_connected
 
-    assert_broadcasts_text "Hello, world!", to: :messages do
-      subscribe_to_broadcasts
+    assert_broadcasts_text "In a section", to: :messages do |text|
+      Message.create(content: text).broadcast_append_to(:important_messages)
     end
   end
 
   private
 
-  def subscribe_to_broadcasts
-    click_on "Start listening for broadcasts"
-
-    assert_no_button "Start listening for broadcasts"
-
-    assert_selector :element, "turbo-cable-stream-source", visible: false
+  def wait_for_stream_to_be_connected
+    assert_selector "turbo-cable-stream-source[connected]", visible: false
   end
 
   def assert_broadcasts_text(text, to:, &block)
