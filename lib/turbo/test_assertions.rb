ea00f3732e21af9c2156cf74dabe95524b17c361
Sean Doyle <seanpdoyle@users.noreply.github.com>
2023-05-08 08:11:15 -0400

    Document the `assert_turbo_stream` helpers (#462)
    First, mention the `assert_turbo_stream` test helpers in the
    [README.md](./README.md) under a new "Testing" heading.
    
    Next, add method-level documentation to describe each helper's
    interface.
    
    Finally, separate the `ActionDispatch::IntegrationTest` portion of the
    test into a new `Turbo::TestAssertions::IntegrationTestAssertions`
    module that's automatically included in
    `ActionDispatch::IntegrationTest` when it's loaded.
    
    By separating the two, the `assert_turbo_stream` helpers can be invoked
    outside of an HTTP request test, while still providing a baseline
    implementation for when an HTTP response is available.

--- lib/turbo/test_assertions.rb
+++ lib/turbo/test_assertions.rb
@@ -7,17 +7,71 @@ module Turbo
       delegate :dom_id, :dom_class, to: ActionView::RecordIdentifier
     end
 
-    def assert_turbo_stream(action:, target: nil, targets: nil, status: :ok, &block)
-      assert_response status
-      assert_equal Mime[:turbo_stream], response.media_type
+    # Assert that the rendered fragment of HTML contains a `<turbo-stream>`
+    # element.
+    #
+    # === Options
+    #
+    # * <tt>:action</tt> [String] matches the element's <tt>[action]</tt>
+    #   attribute
+    # * <tt>:target</tt> [String, #to_key] matches the element's
+    #   <tt>[target]</tt> attribute. If the value responds to <tt>#to_key</tt>,
+    #   the value will be transformed by calling <tt>dom_id</tt>
+    # * <tt>:targets</tt> [String] matches the element's <tt>[targets]</tt>
+    #   attribute
+    #
+    #   Given the following HTML fragment:
+    #
+    #     <turbo-stream action="remove" target="message_1"></turbo-stream>
+    #
+    #   The following assertion would pass:
+    #
+    #     assert_turbo_stream action: "remove", target: "message_1"
+    #
+    # You can also pass a block make assertions about the contents of the
+    # element. Given the following HTML fragment:
+    #
+    #     <turbo-stream action="replace" target="message_1">
+    #       <template>
+    #         <p>Hello!</p>
+    #       <template>
+    #     </turbo-stream>
+    #
+    #   The following assertion would pass:
+    #
+    #     assert_turbo_stream action: "replace", target: "message_1" do
+    #       assert_select "template p", text: "Hello!"
+    #     end
+    #
+    def assert_turbo_stream(action:, target: nil, targets: nil, &block)
       selector =  %(turbo-stream[action="#{action}"])
       selector << %([target="#{target.respond_to?(:to_key) ? dom_id(target) : target}"]) if target
       selector << %([targets="#{targets}"]) if targets
       assert_select selector, count: 1, &block
     end
 
+    # Assert that the rendered fragment of HTML does not contain a `<turbo-stream>`
+    # element.
+    #
+    # === Options
+    #
+    # * <tt>:action</tt> [String] matches the element's <tt>[action]</tt>
+    #   attribute
+    # * <tt>:target</tt> [String, #to_key] matches the element's
+    #   <tt>[target]</tt> attribute. If the value responds to <tt>#to_key</tt>,
+    #   the value will be transformed by calling <tt>dom_id</tt>
+    # * <tt>:targets</tt> [String] matches the element's <tt>[targets]</tt>
+    #   attribute
+    #
+    #   Given the following HTML fragment:
+    #
+    #     <turbo-stream action="remove" target="message_1"></turbo-stream>
+    #
+    #   The following assertion would fail:
+    #
+    #     assert_no_turbo_stream action: "remove", target: "message_1"
+    #
     def assert_no_turbo_stream(action:, target: nil, targets: nil)
-      assert_equal Mime[:turbo_stream], response.media_type
       selector =  %(turbo-stream[action="#{action}"])
       selector << %([target="#{target.respond_to?(:to_key) ? dom_id(target) : target}"]) if target
       selector << %([targets="#{targets}"]) if targets
--- test/streams/streams_controller_test.rb
+++ test/streams/streams_controller_test.rb
@@ -6,7 +6,7 @@ class Turbo::StreamsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to message_path(id: 1)
 
     post messages_path, as: :turbo_stream
-    assert_no_turbo_stream action: :update, target: "messages"
+    assert_no_turbo_stream status: :created, action: :update, target: "messages"
     assert_turbo_stream status: :created, action: :append, target: "messages" do |selected|
       assert_equal "<template>message_1</template>", selected.children.to_html
     end
