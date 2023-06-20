016cf0befb153654ded9700d5ffca1996ada01f6
Hartley McGuire <skipkayhil@gmail.com>
2023-06-20 18:22:20 -0400

    Make turbo_frame_tag fully compatible with dom_id
    Previously, there was an inconsistency with how turbo_frame_tag and
    dom_id would create ids:
    
    ```
    turbo_frame_tag(Article.find(1), "comments") # => <turbo-frame id="article_1_comments" />
    dom_id(Article.find(1), "comments") # => "comments_article_1"
    ```
    
    This commit fixes that incompatibility by fully delegating id generation
    to dom_id when given a model object.
    
    ```
    turbo_frame_tag(Article.find(1), "comments") # => <turbo-frame id="comments_article_1" />
    dom_id(Article.find(1), "comments") # => "comments_article_1"
    
    turbo_frame_tag("comments") # => <turbo-frame id="comments" />
    ```
    
    This does break the use case of passing multiple models, but that seems
    like it would be better fixed in dom_id itself.

--- test/frames/frames_helper_test.rb
+++ test/frames/frames_helper_test.rb
@@ -24,14 +24,7 @@ class Turbo::FramesHelperTest < ActionView::TestCase
   test "string frame nested withing a model frame" do
     record = Article.new(id: 1)
 
-    assert_dom_equal %(<turbo-frame id="article_1_comments"></turbo-frame>), turbo_frame_tag(record, "comments")
-  end
-
-  test "model frame nested withing another model frame" do
-    record = Article.new(id: 1)
-    nested_record = Comment.new
-
-    assert_dom_equal %(<turbo-frame id="article_1_new_comment"></turbo-frame>), turbo_frame_tag(record, nested_record)
+    assert_dom_equal %(<turbo-frame id="comments_article_1"></turbo-frame>), turbo_frame_tag(record, "comments")
   end
 
   test "block style" do
