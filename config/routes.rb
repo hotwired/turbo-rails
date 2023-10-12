16d3412809ec0c20ba93bbc4c9f3e2cb870c916d
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2023-10-12 13:36:30 +0200

    Remove outdated todo
    Done in https://github.com/hotwired/turbo-rails/pull/463/files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-# FIXME: Offer flag to opt out of these native routes
 Rails.application.routes.draw do
   get "recede_historical_location"  => "turbo/native/navigation#recede",  as: :turbo_recede_historical_location
   get "resume_historical_location"  => "turbo/native/navigation#resume",  as: :turbo_resume_historical_location
