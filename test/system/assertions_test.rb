d7c74a1a68f6fde841acd54126edd89f12cd8619
Sean Doyle <sean.p.doyle24@gmail.com>
2024-02-11 10:26:11 -0500

    Introduce `Turbo::SystemTestHelper`
    Introduce the `Turbo::SystemTestHelper` module to be included into
    [ActionDispatch::SystemTestCase][] when it's available.
    
    The module is named to mimic [ActionText::SystemTestHelper][].
    
    The module defines a `#connect_turbo_cable_stream_sources` helper
    method extracted from this project's System Test suite. It aims to
    synchronize the test harness with Turbo's Action Cable-powered broadcast
    support. The method will find all `<turbo-cable-stream-source>` elements
    that are present but not yet `[connected]` (returning the results
    immediately with Capybara's `:wait`), then wait for them to connect
    (using whatever Capybara's configured wait value).
    
    In addition to the `connect_turbo_cable_stream_sources`, also introduce
    a `:turbo_cable_stream_source` Capybara selector, along with
    `assert_turbo_cable_stream_source` and
    `assert_no_turbo_cable_stream_source` helper methods.
    
    [ActionDispatch::SystemTestCase]: https://edgeapi.rubyonrails.org/classes/ActionDispatch/SystemTestCase.html
    [ActionText::SystemTestHelper]: https://edgeapi.rubyonrails.org/classes/ActionText/SystemTestHelper.html

---
+++ test/system/assertions_test.rb
@@ -0,0 +1,34 @@
+require "application_system_test_case"
+
+class AssertionsTest < ApplicationSystemTestCase
+  test "#assert_turbo_cable_stream_source treats the locator as :signed_stream_name filter" do
+    message = Message.new(id: 1)
+
+    visit message_path(message)
+
+    assert_turbo_cable_stream_source message, count: 1
+    assert_no_turbo_cable_stream_source "junk"
+  end
+
+  test "#assert_turbo_cable_stream_source supports String collection filters" do
+    visit messages_path
+
+    assert_turbo_cable_stream_source connected: true, count: 1
+    assert_turbo_cable_stream_source channel: Turbo::StreamsChannel, count: 1
+    assert_turbo_cable_stream_source signed_stream_name: Turbo::StreamsChannel.signed_stream_name("messages"), count: 1
+    assert_no_turbo_cable_stream_source connected: false
+    assert_no_turbo_cable_stream_source channel: "junk"
+    assert_no_turbo_cable_stream_source signed_stream_name: Turbo::StreamsChannel.signed_stream_name("junk")
+  end
+
+  test "#assert_turbo_cable_stream_source supports record filters" do
+    message = Message.new(id: 1)
+
+    visit message_path(message)
+
+    assert_turbo_cable_stream_source signed_stream_name: message
+    assert_turbo_cable_stream_source signed_stream_name: [message]
+    assert_turbo_cable_stream_source signed_stream_name: Turbo::StreamsChannel.signed_stream_name(message)
+    assert_no_turbo_cable_stream_source signed_stream_name: [message, :junk]
+  end
+end
