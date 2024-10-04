7f6e71ff6bb51f82a7e9ed0432bdd738189e3a22
Joe Masilotti <joe@masilotti.com>
2024-10-04 11:38:20 -0700

    Migrate Turbo Native helpers to Hotwire Native

--- app/controllers/turbo/native/navigation.rb
+++ app/controllers/turbo/native/navigation.rb
@@ -1,20 +1,23 @@
 # Turbo is built to work with native navigation principles and present those alongside what's required for the web. When you
-# have Turbo Native clients running (see the Turbo iOS and Turbo Android projects for details), you can respond to native
-# requests with three dedicated responses: <tt>recede</tt>, <tt>resume</tt>, <tt>refresh</tt>.
+# have Hotwire Native clients running (see the Hotwire Native iOS and Hotwire Native Android projects for details),
+# you can respond to native requests with three dedicated responses: <tt>recede</tt>, <tt>resume</tt>, <tt>refresh</tt>.
 #
-# turbo-android handles these actions automatically. You are required to implement the handling on your own for turbo-ios.
+# Hotwire Native Android and Hotwire Native iOS handle these actions automatically.
 module Turbo::Native::Navigation
   extend ActiveSupport::Concern
 
   included do
-    helper_method :turbo_native_app?
+    helper_method :hotwire_native_app?, :turbo_native_app?
   end
 
-  # Turbo Native applications are identified by having the string "Turbo Native" as part of their user agent.
-  def turbo_native_app?
-    request.user_agent.to_s.match?(/Turbo Native/)
+  # Hotwire Native applications are identified by having the string "Hotwire Native" as part of their user agent.
+  # Legacy Turbo Native applications use the "Turbo Native" string.
+  def hotwire_native_app?
+    request.user_agent.to_s.match?(/(Turbo|Hotwire) Native/)
   end
-  
+
+  alias_method :turbo_native_app?, :hotwire_native_app?
+
   # Tell the Turbo Native app to dismiss a modal (if presented) or pop a screen off of the navigation stack. Otherwise redirect to the given URL if Turbo Native is not present.
   def recede_or_redirect_to(url, **options)
     turbo_native_action_or_redirect url, :recede, :to, options
