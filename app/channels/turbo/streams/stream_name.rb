7c2d3743904bc1bba1a4744ae69316ccecdc6877
Nicklas Ramhöj Holtryd <nicklas@37signals.com>
2024-10-22 10:30:55 +0200

    Allow the application to configure Turbo::StreamChannel’s inheritance
    `ApplicationCable::Connection` provides a simple and intuitive way to
    authenticate both custom ActionCable Channels and the
    `Turbo::Broadcastable` broadcasts made on `Turbo::StreamsChannel`.
    
    In multi-tenancy applications simply authenticating the user is often
    not enough as an evicted user could subscribe while being subscribed
    to another account.
    
    This PR allows an application to configure `Turbo::StreamsChannel` super 
    class with the intention of allowing application specific authorization
    logic to be implemented in e.g `ApplicationCable::Channel`.
    
    `ApplicationCable::Channel` is generated by rails (new and action cable)
    but could have been removed. It’s also possible that
    `ApplicationCable::Channel` implements `authorized?` in a way that’s not
    meant to be used by `Turbo::StreamsChannel`. Do avoid this from being
    a breaking change `Turbo::StreamsChannel`’s super class defaults to
    `ActionCable::Channel::Base` (the current behavior).
    
    User can opt-in to inheriting from `ApplicationCable::Channel` (or any
    other Channel) with:
    
    ```rb
    config.turbo.base_stream_channel_class = "ApplicationCable::Channel"
    ```
    
    Example of attacks possible in a multi-tenancy application when a
    Channel is streaming from a `Turbo::Broadcastable` compatible stream
    name:
    
    1. A user uses the browser’s “Save as…” and sends the html file to
       a colleague or is tricked to send it directly to a bad actor.
       Since `turbo_stream_from` makes the signed stream name appear in
       the HTML the HTML needs to be treated as a secret similar to if a
       password or API token was present.
    2. Someone with access to a shared stream can save the signed stream
       name. If the user is later removed from the account with the
       expectation to loose access to the shared stream they can sign up
       for another account and stream from there even with an authentication
       check in place.
    
    Some reasons why attacks are more likely / more severe.
    
    1. Stealing a signed token name is could be more practical, easier
       and/or faster than e.g stealing cookies.
    2. Web Socket connection and subscription logs might not be as well
       tracked as HTTP logs leading to breaches being harder to discover.
    3. Less is published on Web Socket security than HTTP and awareness of
       the need to authenticate and authorize Web Sockets may be lower than
       that of HTTP.
    4. Signed Stream Names never expire and could go unnoticed for a long
       time. Leaking an old stream name doesn’t have the same protection as
       e.g leaking an old password reset token has.
    
    
    I believe we should move towards making this the default as a way to
    encourage securing `Turbo::Broadcastable` broadcasts in the following
    steps:
    
    1. This PR. No encouragements but a cleaner way and documented way to
       implement authorization.
    2. Generate new rails applications with:
       `config.turbo.base_stream_channel_class = "ApplicationCable::Channel"`
       and include a commented-out authentication example in the
       `ApplicationCable::Connection` template and a commented-out
       authorization example in `ApplicationCable::Channel` template.
    4. Change the default value of `base_stream_channel_class` to 
       `"ApplicationCable::Channel"` and update rails new to generate the
       opposite for those wishing to opt-out.
       `config.turbo.base_stream_channel_class = "ActionCable::Channel::Base"`
    5. Remove the configuration and hard code `ApplicationCable::Channel` as
       `Turbo::StreamsChannel`’s super class.

--- app/channels/turbo/streams/stream_name.rb
+++ app/channels/turbo/streams/stream_name.rb
@@ -2,7 +2,12 @@
 # <tt>Turbo::StreamsChannel</tt>, but each with their own subscription. Since stream names are exposed directly to the user
 # via the HTML stream subscription tags, we need to ensure that the name isn't tampered with, so the names are signed
 # upon generation and verified upon receipt. All verification happens through the <tt>Turbo.signed_stream_verifier</tt>.
+#
+# Signed stream names do not expire. To prevent unauthorized access through leaked stream names it is recommended to
+# authorize subscriptions and/or authenticate connections based on your needs.
 module Turbo::Streams::StreamName
+  STREAMABLE_SEPARATOR = ":"
+
   # Used by <tt>Turbo::StreamsChannel</tt> to verify a signed stream name.
   def verified_stream_name(signed_stream_name)
     Turbo.signed_stream_verifier.verified signed_stream_name
@@ -14,16 +19,23 @@ module Turbo::Streams::StreamName
   end
 
   module ClassMethods
-    # Can be used by custom turbo stream channels to obtain signed stream name from <tt>params</tt>
+    # Can be used by <tt>config.turbo.base_stream_channel_class</tt> or a custom channel to obtain signed stream name
+    # from <tt>params</tt>.
     def verified_stream_name_from_params
       self.class.verified_stream_name(params[:signed_stream_name])
+    end
+
+    # Can be used by <tt>config.turbo.base_stream_channel_class</tt> or a custom channel to obtain signed stream name
+    # parts from <tt>params</tt>.
+    def verified_stream_name_parts_from_params
+      verified_stream_name_from_params.split STREAMABLE_SEPARATOR
     end
   end
 
   private
     def stream_name_from(streamables)
       if streamables.is_a?(Array)
-        streamables.map  { |streamable| stream_name_from(streamable) }.join(":")
+        streamables.map  { |streamable| stream_name_from(streamable) }.join(STREAMABLE_SEPARATOR)
       else
         streamables.then { |streamable| streamable.try(:to_gid_param) || streamable.to_param }
       end
