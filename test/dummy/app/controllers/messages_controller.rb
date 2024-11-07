0e42702df40698521cc7e506fea927bc3a2a08ab
Petrik de Heus <petrik@deheus.net>
2024-11-07 22:44:22 +0100

    Keep flash in Turbo Frame requests (#699)
    A POST request can set the flash and return a redirect response.
    If a Turbo-Frame request that gets made before the redirect location
    gets called, the flash gets discared by the Turbo-Frame request.
    
    By making sure a Turbo-Frame request keeps the flash, we can avoid this
    problem.

--- test/dummy/app/controllers/messages_controller.rb
+++ test/dummy/app/controllers/messages_controller.rb
@@ -16,7 +16,7 @@ class MessagesController < ApplicationController
 
   def create
     respond_to do |format|
-      format.html { redirect_to message_url(id: 1) }
+      format.html { redirect_to message_url(id: 1), notice: "Message was successfully created." }
       format.turbo_stream { render turbo_stream: turbo_stream.append(:messages, "message_1"), status: :created }
     end
   end
