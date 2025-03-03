bc499f98ee1f3f15dd9077b17af2d8030776ba76
Vlad Yarotsky <vladimir.yarotsky@gmail.com>
2025-03-03 07:54:39 -0800

    Fix unwanted model retention when using class methods from `Turbo::Broadcastable` with locals (#710)
    Consider the following model:
    
    ```ruby
    class MyModel < ApplicationRecord
      broadcasts locals: { something_static: true }
    end
    ```
    
    When broadcasting for the first time without this fix, Broadcastable
    does something like:
    
    ```
    { something_static }.reverse_merge!(my_model: #<MyModel: id=1>)
    ```
    
    On the second broadcast for another model instance, the reverse merge
    will be a no-op, because the first model is already in the Hash!
    
    Switching to a non-destructive `reverse_merge` resolves the issue.

--- test/dummy/app/models/comment.rb
+++ test/dummy/app/models/comment.rb
@@ -5,5 +5,6 @@ class Comment < ApplicationRecord
 
   broadcasts_to ->(comment) { [comment.article, :comments] },
     target: ->(comment) { "article_#{comment.article_id}_comments" },
-    partial: "comments/different_comment"
+    partial: "comments/different_comment",
+    locals: { highlight: true }
 end
