5744f2ecd9ba9097110cf3f10981f52f6d56c272
Jorge Manrubia <jorge@37signals.com>
2025-03-03 14:06:11 +0100

    Revert "Keep flash in Turbo Frame requests (#699)"
    This reverts commit 0e42702df40698521cc7e506fea927bc3a2a08ab.

--- test/dummy/app/controllers/messages_controller.rb
+++ test/dummy/app/controllers/messages_controller.rb
@@ -16,7 +16,7 @@ class MessagesController < ApplicationController
 
   def create
     respond_to do |format|
-      format.html { redirect_to message_url(id: 1), notice: "Message was successfully created." }
+      format.html { redirect_to message_url(id: 1) }
       format.turbo_stream { render turbo_stream: turbo_stream.append(:messages, "message_1"), status: :created }
     end
   end
