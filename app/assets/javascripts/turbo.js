8241ae3fe603588315a1a544faed171a300d1e22
Adrien Poly <adrienpoly@gmail.com>
2025-03-04 20:00:27 +0900

    Update @rails/actioncable dependency to version >=7.0 (#705)
    * Update @rails/actioncable dependency to version >=7.0
    
    * yarn build

--- app/assets/javascripts/turbo.js
+++ app/assets/javascripts/turbo.js
@@ -5490,8 +5490,8 @@ window.Turbo = Turbo$1;
 addEventListener("turbo:before-fetch-request", encodeMethodIntoRequestBody);
 
 var adapters = {
-  logger: self.console,
-  WebSocket: self.WebSocket
+  logger: typeof console !== "undefined" ? console : undefined,
+  WebSocket: typeof WebSocket !== "undefined" ? WebSocket : undefined
 };
 
 var logger = {
@@ -5533,12 +5533,11 @@ class ConnectionMonitor {
   isRunning() {
     return this.startedAt && !this.stoppedAt;
   }
-  recordPing() {
+  recordMessage() {
     this.pingedAt = now();
   }
   recordConnect() {
     this.reconnectAttempts = 0;
-    this.recordPing();
     delete this.disconnectedAt;
     logger.log("ConnectionMonitor recorded connect");
   }
@@ -5737,6 +5736,7 @@ Connection.prototype.events = {
       return;
     }
     const {identifier: identifier, message: message, reason: reason, reconnect: reconnect, type: type} = JSON.parse(event.data);
+    this.monitor.recordMessage();
     switch (type) {
      case message_types.welcome:
       if (this.triedToReconnect()) {
@@ -5752,7 +5752,7 @@ Connection.prototype.events = {
       });
 
      case message_types.ping:
-      return this.monitor.recordPing();
+      return null;
 
      case message_types.confirmation:
       this.subscriptions.confirmSubscription(identifier);
