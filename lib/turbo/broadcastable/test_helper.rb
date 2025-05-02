47fd82f618d23f4984a448fc1d74950951c8f9be
Tyler Cosgrove <Vivalldi@users.noreply.github.com>
2025-05-02 17:42:04 -0400

    Only return messages produced by block in `capture_turbo_stream_broadcasts`

--- lib/turbo/broadcastable/test_helper.rb
+++ lib/turbo/broadcastable/test_helper.rb
@@ -155,10 +155,13 @@ module Turbo
       #     assert_equal "replace", remove["action"]
       #
       def capture_turbo_stream_broadcasts(stream_name_or_object, &block)
-        block&.call
-
         stream_name = stream_name_from(stream_name_or_object)
-        payloads = broadcasts(stream_name)
+
+        if block_given?
+          payloads = new_broadcasts_from(broadcasts(stream_name), stream_name, "capture_turbo_stream_broadcasts", &block)
+        else
+          payloads = broadcasts(stream_name)
+        end
 
         payloads.flat_map do |payload|
           html = ActiveSupport::JSON.decode(payload)
